4c3f48ff058ac378d00ab4ca0d0d513e5a00c7ca
Aleksandr <soft@vhlv.ru>
2021-11-14 14:39:18 +0300

    сверстал About

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Promo from "../Promo/Promo";
+import About from "../About/About";
+
 import './Main.css';
 
 function Main() {
   return (
     <main className="main">
       <Promo />
+      <About />
     </main>
   );
 }
