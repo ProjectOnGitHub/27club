e44c4cc38297fa58b8c7acfad66a96f53e5d43d3
Aleksandr <soft@vhlv.ru>
2021-11-18 15:50:28 +0300

    сделал 404 страницу

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import { Route, Switch, useHistory } from 'react-router';
 import Helmet from "react-helmet"
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import PageNotFound from '../PageNotFound/PageNotFound';
 import './App.css';
 
 function App() {
+  const history = useHistory();
+  function handleGoBack() {
+    history.goBack();
+
+  }
   return (
     <div className="app">
-      <Helmet
-        title="Клуб 27"
-      />
-      <Header />
-      <Main />
-      <Footer />
+      <Switch>
+        <Route exact path="/">
+          <Helmet
+            title="Клуб 27"
+          />
+          <Header />
+          <Main />
+          <Footer />
+        </Route>
+        <Route path="*">
+          <Helmet
+            title="Страница не найдена"
+          />
+          <PageNotFound
+            handleGoBack={handleGoBack}
+          />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './PageNotFound.css';
+import { Link } from 'react-router-dom';
+
+// eslint-disable-next-line react/prop-types
+function PageNotFound({ handleGoBack }) {
+  return (
+    <main className="page-not-found">
+      <div className="page-not-found__content">
+        <h1 className="page-not-found__status">404</h1>
+        <p className="page-not-found__text">Страница не найдена</p>
+        <Link className="page-not-found__link hover-effect" onClick={handleGoBack}>Назад</Link>
+      </div>
+    </main>
+  )
+}
+
+export default PageNotFound;
