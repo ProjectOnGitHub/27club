f1d5c68fa72ae1b6e441df3f79812cce22981212
Aleksandr <soft@vhlv.ru>
2021-11-30 00:47:50 +0300

    Добавил тайтлы на страницы

--- src/components/AboutMember/AboutMember.js
+++ src/components/AboutMember/AboutMember.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-//import Helmet from "react-helmet"
+import Helmet from "react-helmet"
 //import './AboutMember.css';
 
 function AboutMember({ name, location, url, image }) {
@@ -11,12 +11,15 @@ function AboutMember({ name, location, url, image }) {
   return (
     <>
       {location.pathname === `/${url}` ? (
-        <section className="about">
-          <article className="about__info section" >
-            <h1 className="about__title">{name}</h1>
-            <img src={image} alt={name} />
-          </article>
-        </section>
+        <>
+          <Helmet title={name} />
+          <section className="about">
+            <article className="about__info section" >
+              <h1 className="about__title">{name}</h1>
+              <img src={image} alt={name} />
+            </article>
+          </section>
+        </>
       ) : (
         ""
       )
